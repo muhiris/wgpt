e0bc803f3f6e0dd3f9948c63733367218e09f940
muhiris <muhammadharis786@protonmail.com>
2023-08-06 13:44:10 +0500

    Issue Resolved: Change API

--- index.js
+++ index.js
@@ -1,97 +1,92 @@
-#!/usr/bin/env node
-import axios from "axios";
+import { chromium } from "playwright";
 import clipboardy from "clipboardy";
 
-// Read the command line arguments
-const args = process.argv.slice(2);
+const url = "https://aichatonline.org/chat/";
+const DEFAULT_TIMEOUT_SECONDS = 7;
 
-// Check for --help flag
-if (args.includes("--help")) {
-  console.log('Usage: node app.js [OPTIONS] "Prompt Text "\n');
-  console.log("Options:");
-  console.log(
-    '-h: Add "Rephrase this sentence in 3 different ways " at the start of the input'
-  );
-  console.log('-c: Add "Write code for" at the start of the input');
-  console.log(
-    "--no-clipboard: Turn off copying the response to the system clipboard"
-  );
-  console.log("--help: Show usage instructions");
-  process.exit(0);
+async function scrape(message, timeoutSeconds) {
+  try {
+    const browser = await chromium.launch({ headless: true }); // Run with a visible browser window
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    // Navigate to the chat URL
+    await page.goto(url);
+
+    // Type the provided message into the textarea
+    await page.fill("textarea", message);
+
+    // Click the 'Send' button
+    await page.press("textarea", "Enter");
+
+    // Wait for the specified or default timeout for the response to be generated
+    await page.waitForTimeout(
+      (timeoutSeconds || DEFAULT_TIMEOUT_SECONDS) * 1000
+    );
+
+    // Extract the most recent response div
+    const mostRecentResponse = await page.evaluate(() => {
+      const div = document.querySelector(".mwai-reply.mwai-ai:last-child");
+      return div ? div.textContent : null;
+    });
+
+    // Remove the word "AI" from the beginning of the response
+    const cleanedResponse = mostRecentResponse.replace(/^AI/, "");
+
+    // Log the cleaned response
+    console.log(cleanedResponse.trim());
+
+    if (message.startsWith("Rephrase text in 3 ways: ")) {
+      clipboardy.writeSync(cleanedResponse.trim());
+      console.log("Result copied to the clipboard.");
+    }
+
+    // Close the browser
+    await browser.close();
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
 }
 
-// If no arguments provided, show usage instructions
-if (args.length === 0) {
+// Read command-line arguments
+const args = process.argv.slice(2);
+let message = null;
+let timeoutSeconds = null;
+
+// Parse command-line arguments
+for (let i = 0; i < args.length; i++) {
+  const arg = args[i];
+  if (arg === "--help" || arg === "-h") {
+    console.log("Usage: wgpt [message] [--timeout seconds] [-p] [-c]");
+    console.log("Example 1: wgpt 'Hello, AI!'");
+    console.log("Example 2: wgpt 'Hello, AI!' --timeout 10");
+    console.log("Example 3: wgpt 'Hello, AI!' -p");
+    console.log("Example 4: wgpt 'Write code to generate prime numbers' -c");
+    process.exit(0);
+  } else if (arg === "--timeout") {
+    if (i + 1 < args.length) {
+      timeoutSeconds = parseInt(args[i + 1]);
+      i++; // Skip the next argument since it was used for the timeout value
+    } else {
+      console.log(
+        "Invalid use of --timeout. Please provide a valid number for the timeout."
+      );
+      process.exit(1);
+    }
+  } else if (arg === "-p") {
+    message = "Rephrase text in 3 ways: " + message;
+  } else if (arg === "-c") {
+    message = "Write code to: " + message;
+  } else {
+    message = arg;
+  }
+}
+
+if (!message) {
   console.log(
-    "Error: No arguments provided. Use --help for usage instructions."
+    "Please provide a message as a command-line argument or type --help for more information."
   );
   process.exit(1);
 }
 
-let prompt = args.join(" ");
-
-// Check for flags
-let rephraseFlag = false;
-let writeCodeFlag = false;
-let clipboardFlag = true;
-
-if (args.includes("-h")) {
-  rephraseFlag = true;
-  prompt = "Rephrase this sentence in 3 different ways " + prompt;
-}
-
-if (args.includes("-c")) {
-  writeCodeFlag = true;
-  prompt = "Write code for " + prompt;
-}
-
-if (args.includes("--no-clipboard")) {
-  clipboardFlag = false;
-}
-
-// Prepare the data payload
-const data = {
-  prompt: prompt,
-};
-
-// Set the headers
-const headers = {
-  "User-Agent":
-    "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0",
-  Accept: "application/json, text/plain, */*",
-  "Accept-Language": "en-US,en;q=0.5",
-  "Content-Type": "application/json",
-  Origin: "https://chatbot.theb.ai",
-  Referer: "https://chatbot.theb.ai/",
-};
-
-// Send the POST request
-const url = "https://chatbot.theb.ai/api/chat-process";
-
-axios
-  .post(url, data, { headers })
-  .then((response) => {
-    if (response.status === 200) {
-      const response_text = response.data;
-
-      // Find the last JSON string in the response text
-      const json_strings = response_text.trim().split("\n");
-      const last_json_string = json_strings[json_strings.length - 1];
-
-      const response_json = JSON.parse(last_json_string);
-      const outputText = response_json.text;
-
-      console.log(outputText);
-
-      // Copy to clipboard if -h flag is used and clipboardFlag is true
-      if (rephraseFlag && clipboardFlag) {
-        clipboardy.writeSync(outputText);
-        console.log("Response copied to clipboard.");
-      }
-    } else {
-      console.log("Error:", response.status);
-    }
-  })
-  .catch((error) => {
-    console.log("Error:", error.message);
-  });
+scrape(message, timeoutSeconds);
