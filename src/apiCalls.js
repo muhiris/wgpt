3ba69f1c9c0d3d80e147bdeec36a11864a5e2de3
muhiris <muhammadharis786@protonmail.com>
2023-08-19 14:57:49 +0500

    Made Data from Stream

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,19 +1,65 @@
 import axios from "axios";
 import ora from "ora";
 const endpoint = "https://free.churchless.tech/v1/chat/completions";
-const spinner = ora("Loading...");
+
+let responseBuffer = "";
 
 export async function getResponseFromAPI(userInput, showSpinner = true) {
   try {
     const spinner = showSpinner ? ora("Loading...").start() : null;
-    const response = await axios.post(endpoint, {
-      messages: [{ role: "user", content: userInput }],
-    });
+    const response = await getResponse(userInput);
     if (spinner) {
       spinner.stop();
     }
-    return response.data.choices[0].message.content;
+    const reader = response.data;
+
+    reader.on("data", (chunk) => {
+      responseBuffer += chunk.toString();
+      processResponses();
+    });
+
+    reader.on("end", () => {
+      processResponses();
+    });
   } catch (error) {
     throw new Error(error.message);
+  }
+}
+
+async function getResponse(userInput) {
+  let res = await axios.post(
+    endpoint,
+    {
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: userInput }],
+      stream: true,
+    },
+    {
+      responseType: "stream",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return res;
+}
+function processResponses() {
+  const responseList = responseBuffer.split("data:"); // Split responses by "data:"
+  responseBuffer = responseList.pop(); // Store any incomplete response for the next iteration
+
+  for (const response of responseList) {
+    const contentStartIndex = response.indexOf('"content":"');
+    if (contentStartIndex !== -1) {
+      const contentEndIndex = response.indexOf('"', contentStartIndex + 11);
+      if (contentEndIndex !== -1) {
+        const contentValue = response
+          .substring(contentStartIndex + 11, contentEndIndex)
+          .replace(/\\n/g, "\n") // Replace "\\n" with newline characters
+          .replace(/\\t/g, "\t")
+          .replace(/\n6$/, "");
+
+        process.stdout.write(contentValue);
+      }
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,15 +3,13 @@ import { getResponseFromAPI } from "./apiCalls.js";
 import { parseCommandLineArguments } from "./aurgumentsHandler.js";
 import { displayUsage, displayVersion } from "./display.js";
 
-// Function to print words with animation
-async function printWordsWithAnimation(words, delay) {
-  for (const word of words) {
-    await new Promise((resolve) => setTimeout(resolve, delay));
-    process.stdout.write(word + " "); // Print word with space
+async function processResponse(userInput, spinner = true) {
+  const content = await getResponseFromAPI(userInput, spinner);
+  if (content !== undefined) {
+    const words = content.split(" "); // Split content into words
+    process.stdout.write(words);
   }
-  console.log(); // Print a new line after animation
 }
-
 export async function main() {
   const userArguments = process.argv.slice(2);
   const parsedArguments = parseCommandLineArguments(userArguments);
@@ -23,24 +21,15 @@ export async function main() {
   }
   if (parsedArguments.interactiveMode) {
     console.log("Interactive mode. Enter 'exit' to quit.");
-
     while (true) {
       const userInput = await getUserInput();
-
       if (userInput.toLowerCase() === "exit") {
         console.log("\nExiting interactive mode.");
         break;
       }
-
-      try {
-        const content = await getResponseFromAPI(userInput, false);
-        const words = content.split(" "); // Split content into words
-        const animationDelay = 30; // Adjust the delay as needed
-
-        await printWordsWithAnimation(words, animationDelay);
-      } catch (error) {
-        console.error("Error:", error.message);
-      }
+      await processResponse(userInput, false).catch((err) => {
+        console.log(`Error: ${err.message}`);
+      });
     }
   } else if (parsedArguments.versionFlag) {
     displayVersion();
@@ -52,38 +41,18 @@ export async function main() {
       closeReadline();
       return;
     }
-
-    const flag = parsedArguments.codeFlag ? "-c" : "-p";
+    const flag = parsedArguments.codeFlag
+      ? "Write code for "
+      : "Repharase it in 3 ways: ";
     const userInput = parsedArguments.messageContent;
-
-    try {
-      const content = await getResponseFromAPI(
-        `${
-          parsedArguments.codeFlag
-            ? "Write code for "
-            : "Rephrase it in 3 ways: "
-        } "${userInput}"`
-      );
-      const words = content.split(" "); // Split content into words
-      const animationDelay = 30; // Adjust the delay as neededs
-
-      await printWordsWithAnimation(words, animationDelay);
-    } catch (error) {
-      console.error("Error:", error.message);
-    }
+    processResponse(flag + userInput).catch((err) => {
+      console.error("Error:", err.message);
+    });
   } else {
     const userInput = parsedArguments.userInput;
-
-    try {
-      const content = await getResponseFromAPI(userInput);
-      const words = content.split(" "); // Split content into words
-      const animationDelay = 30; // Adjust the delay as needed
-
-      await printWordsWithAnimation(words, animationDelay);
-    } catch (error) {
-      console.error("Error:", error.message);
-    }
+    processResponse(userInput).catch((err) => {
+      console.error("Error:", err.message);
+    });
   }
-
   closeReadline();
 }
